6818fc6b5c347fde2b8c338d5c1397690c2d1532
Reiss Cashmore <reiss.cashmore@outlook.com>
2024-09-05 02:50:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,11 +7,6 @@ const { handleStart, handlePagination } = require('./src/routes');
 var util = require('util')
 
 Apify.main(async () => {
-    const requestList = new Apify.RequestList({
-        sources: [{ url: 'https://www.kickstarter.com/projects/romain-p/modular-wizard-tower-1?ref=discovery&term=modular-wizard-tower-1&total_hits=1&category_id=34' }],
-    });
-    await requestList.initialize();
-
     const input = await Apify.getInput();
     const queryParameters = await parseInput(input);
     let { maxResults } = input;
@@ -25,6 +20,7 @@ Apify.main(async () => {
     // Playwright Crawler Configuration
     const crawler = new Apify.PlaywrightCrawler({
         requestList,
+        headless: false,
         maxConcurrency: 1,
         useSessionPool: true,
         launchContext: {
@@ -34,27 +30,26 @@ Apify.main(async () => {
                 proxy: proxy ? { server: proxy.newUrl() } : undefined, // Use proxy if configured
             },
         },
-        handlePageFunction: async ({ request, page, browserController }) => {
+        requestHandler: async ({ parseWithCheerio, request, enqueueLinks }) => {
             const { url, userData: { label } } = request;
             log.info('Page opened.', { label, url });
 
             // Use Playwright's page object to interact with the website
-            await page.waitForSelector('body'); // Ensure page has loaded
-
-            // Extracting content from the page (modify this according to your scraping needs)
-            const bodyHTML = await page.content();
-
+            const $ = await parseWithCheerio();
             // Optionally, you can log the page content for debugging
-            console.log(util.inspect(bodyHTML));
+            console.log(util.inspect($('body')));
 
             // Push data to the Apify dataset
-            await Apify.pushData({ bodyHTML });
+            await Apify.pushData({ body: $('body') });
         },
         handleFailedRequestFunction: async ({ request, error }) => {
             log.error(`Request ${request.url} failed repeatedly, running out of retries (Error: ${error.message})`);
         },
     });
-
+    await crawler.addRequests([{
+        url: 'https://www.kickstarter.com/projects/romain-p/modular-wizard-tower-1?ref=discovery&term=modular-wizard-tower-1&total_hits=1&category_id=34',
+        label: 'start-url',
+    }]);
     log.info('Starting crawler');
     await crawler.run();
     log.info('Crawler finished');
