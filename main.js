325f2e01d6310c96b1f3ed15cf9df0c4a81902b6
Reiss Cashmore <reiss.cashmore@outlook.com>
2024-09-05 01:36:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ Apify.main(async () => {
                 },
                 responseType: 'json',
             });
-            console.log(console.log(util.inspect(response)))
+           console.log(util.inspect(response.body))
         },
         handleFailedRequestFunction: async ({
             request,
